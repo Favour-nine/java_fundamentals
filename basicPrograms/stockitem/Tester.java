e2722c13836300c45fda608f8cccb3630e439007
ayobami7 <adeyemoayobami300@yahoo.com>
2025-02-18 10:59:38 +0000

    update

--- basicPrograms/stockitem/Tester.java
+++ basicPrograms/stockitem/Tester.java
@@ -2,8 +2,8 @@ package basicPrograms.stockitem;
 
 public class Tester {
     public static void main(String[] args) {
-        StockItem bob = new StockItem("Baked Beans");
-        bob.descript;
+        StockItem bob = new StockItem("Baked Beans", 12, 65);
+
 
         System.out.println("******");
     }
