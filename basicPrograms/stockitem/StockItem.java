78920ac14e9989a2989a738d3e87938859352630
ayobami7 <adeyemoayobami300@yahoo.com>
2025-02-22 19:36:26 +0000

    update

--- basicPrograms/stockitem/StockItem.java
+++ basicPrograms/stockitem/StockItem.java
@@ -1,40 +1,29 @@
 package basicPrograms.stockitem;
-import java.util.Scanner;
 
 class StockItem {
     private String description;
     private int level;
     private double price;
 
-    Scanner sc = new Scanner(System.in);
 
-    StockItem(String descript, int lv, double itemPrice) {
-        description = descript;
-        level = lv;
-        price = itemPrice;
 
-//        System.out.print("Enter description: ");
-//        description= sc.nextLine();
-//        System.out.print("Enter level: ");
-//        level= sc.nextInt();
-//        System.out.print("Enter price: ");
-//        price= sc.nextDouble();
-
-//        sc.close();
-
+    StockItem(String description, int level, double price) {
+        this.description = description;
+        this.level = level;
+        this.price = price;
     }
 
-    public static void printReport(){
-        StockItem item = new StockItem();
+    public void printReport(){
         System.out.println("****Stock Report*****");
-        System.out.printf("Item: %s", item.description);
-        System.out.printf("\nStock Level: %d",item.level);
-        System.out.printf("\nPrice: %.1fp",item.price);
-        System.out.println("\n*********");
+        System.out.printf("Item: %s%n", this.description);
+        System.out.printf("Stock Level: %d%n",this.level);
+        System.out.printf("Price: %.1fp%n",this.price);
+        System.out.println("*********");
     }
 
     public static void main(String[] args){
-        printReport();
+        StockItem item = new StockItem("Baked beans", 15, 65);
+        item.printReport();
         System.out.print("done ");
 
     }
--- basicPrograms/stockitem/Tester.java
+++ basicPrograms/stockitem/Tester.java
@@ -3,8 +3,6 @@ package basicPrograms.stockitem;
 public class Tester {
     public static void main(String[] args) {
         StockItem bob = new StockItem("Baked Beans", 12, 65);
-
-
-        System.out.println("******");
+        bob.printReport();
     }
 }
