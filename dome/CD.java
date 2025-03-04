149bb408c711dea017acfcba8dcff38d5e0100a0
ayobami7 <adeyemoayobami300@yahoo.com>
2025-03-04 20:57:41 +0000

    practical 5

---
+++ dome/CD.java
@@ -0,0 +1,78 @@
+package dome;
+
+public class CD extends Item{
+
+    private String artist;
+    private int numberOfTracks;
+
+    /**
+     * Initialize the CD.
+     * @param theTitle The title of the CD.
+     * @param theArtist The artist of the CD.
+     * @param tracks The number of tracks on the CD.
+     * @param time The playing time of the CD.
+     */
+    public CD(String theTitle, String theArtist, int tracks, int time)
+    {
+        super(theTitle, time);
+        artist = theArtist;
+        numberOfTracks = tracks;
+    }
+
+
+    /**
+     * @return the artist
+     */
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    /**
+     * sets a new artist
+     * @dir the new artist
+     */
+    public void setArtist(String dir)
+    {
+        artist = dir;
+    }
+
+    /**
+     * @return the number of tracks
+     */
+    public int getTracks()
+    {
+        return numberOfTracks;
+    }
+
+    /**
+     * sets number of tracks
+     * @trks number of tracks
+     */
+    public void setTracks(int trks)
+    {
+        numberOfTracks = trks;
+    }
+
+
+    /**
+     * Print details about this CD to the text terminal.
+     */
+    public void print()
+    {
+//        System.out.print("CD: " + title + " (" + playingTime + " mins)");
+//        if(gotIt)
+//        {
+//            System.out.println("*");
+//        }
+//        else
+//        {
+//            System.out.println();
+//        }
+        System.out.println("    " + artist);
+        System.out.println("    tracks: " + numberOfTracks);
+//        System.out.println("    " + comment);
+    }
+
+
+}
---
+++ dome/DVD.java
@@ -0,0 +1,60 @@
+package dome;
+
+/**
+ * The DVD class represents a DVD object. Information about the 
+ * DVD is stored and can be retrieved. We assume that we only deal 
+ * with movie DVDs at this stage.
+ * 
+ */
+public class DVD extends Item
+{
+    private String director;
+
+
+    /**
+     * Constructor for objects of class DVD
+     * @param theTitle The title of this DVD.
+     * @param theDirector The director of this DVD.
+     * @param time The running time of the main feature.
+     */
+    public DVD(String theTitle, String theDirector, int time)
+    {
+        super(theTitle, time);
+        director = theDirector;
+    }
+
+    /** 
+     * @return the director
+     */
+    public String getDirector()
+    {
+        return director;
+    }
+    
+    /** 
+     * sets a new director
+     * @dir the new director
+     */
+    public void setDirector(String dir)
+    {
+        director = dir;
+    }
+    
+    /**
+     * Print details about this DVD to the text terminal.
+     */
+    public void print()
+    {
+//        System.out.print("DVD: " + title + " (" + playingTime + " mins)");
+//        if(gotIt)
+//        {
+//            System.out.println("*");
+//        }
+//        else
+//        {
+//            System.out.println();
+//        }
+        System.out.println("    " + director);
+//        System.out.println("    " + comment);
+    }
+}
